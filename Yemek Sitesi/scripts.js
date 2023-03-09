768de07b20bffcd3569d8e1a85366347dbd804fc
uKaya <ufuk.kayaa52@gmail.com>
2023-03-09 17:34:50 +0300

    Yemek Tarif Site Template
    Yemek tarif için temel düzey bir yemek tarif sitesi.

---
+++ Yemek Sitesi/scripts.js
@@ -0,0 +1,58 @@
+/*!
+* Start Bootstrap - Agency v7.0.10 (https://startbootstrap.com/theme/agency)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink fonksiyonu (aşağı kaydırınca üste menuyu uste sabıtleme)
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#menu');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+
+    navbarShrink();
+
+    
+    document.addEventListener('scroll', navbarShrink);
+
+    
+    const menu = document.body.querySelector('#menu');
+    if (menu) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#menu',
+            offset: 74,
+        });
+    };
+
+    
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+$(document).ready(function() {      
+   $('.carousel').carousel('pause');
+});
